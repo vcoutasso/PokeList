8b4fd6297d3820deba13e5b9fe27f4d637c0f12a
Vinícius Couto Tasso <vcoutasso@gmail.com>
2022-06-21 22:40:08 -0300

    chore: organize mvp pattern file structure

--- PokeList/PokeList/Lifecycle/SceneDelegate.swift
+++ PokeList/PokeList/Lifecycle/SceneDelegate.swift
@@ -12,7 +12,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = PokemonListViewController()
         window?.makeKeyAndVisible()
     }
 
